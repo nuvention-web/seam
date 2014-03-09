be1d1c952af5c4c4caf01f56cc8cc19b32710d4b
MikeTYChen <michael753@gmail.com>
2014-03-08 19:54:44 -0600

    attendance tracker

---
+++ public/javascripts/meetings-attendance.js
@@ -0,0 +1,21 @@
+//FUNCTIONS FOR PROGRESS BAR DURING MEETING
+$(document).ready(function(){ 
+
+
+});
+
+function changeAttendance(ID){
+	var current=document.getElementById(ID).innerHTML;
+	var IDElement='#'+ID;
+	if(current=='ABSENT'){
+		document.getElementById(ID).innerHTML='PRESENT';
+		$(IDElement).removeClass('text-orange-blue-hover');
+		$(IDElement).addClass('text-light-blue-nohover');
+
+	}else{
+		document.getElementById(ID).innerHTML='ABSENT';
+		$(IDElement).removeClass('text-light-blue-nohover');
+		$(IDElement).addClass('text-orange-blue-hover');
+	}
+
+}
