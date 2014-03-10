272da135f8fb44896c2cd65e024733acaf30661a
jyu3138 <tyu3138@gmail.com>
2014-03-10 16:22:34 -0500

    working on redirect uri

--- config/passport.js
+++ config/passport.js
@@ -9,122 +9,122 @@ var User = require('../models/user-model');
 // expose this function to our app using module.exports
 module.exports = function(passport) {
 
-    // =========================================================================
-    // passport session setup ==================================================
-    // =========================================================================
-    // required for persistent login sessions
-    // passport needs ability to serialize and unserialize users out of session
+	// =========================================================================
+	// passport session setup ==================================================
+	// =========================================================================
+	// required for persistent login sessions
+	// passport needs ability to serialize and unserialize users out of session
 
-    // used to serialize the user for the session
-    passport.serializeUser(function(user, done) {
-        done(null, user.id);
-    });
+	// used to serialize the user for the session
+	passport.serializeUser(function(user, done) {
+		done(null, user.id);
+	});
 
-    // used to deserialize the user
-    passport.deserializeUser(function(id, done) {
-        User.findById(id, function(err, user) {
-            done(err, user);
-        });
-    });
+	// used to deserialize the user
+	passport.deserializeUser(function(id, done) {
+		User.findById(id, function(err, user) {
+			done(err, user);
+		});
+	});
 
- 	// =========================================================================
-    // LOCAL SIGNUP ============================================================
-    // =========================================================================
-    // we are using named strategies since we have one for login and one for signup
+	// =========================================================================
+	// LOCAL SIGNUP ============================================================
+	// =========================================================================
+	// we are using named strategies since we have one for login and one for signup
 	// by default, if there was no name, it would just be called 'local'
 
-    passport.use('local-signup', new LocalStrategy({
-        // by default, local strategy uses username and password, we will override with email
-        usernameField : 'email',
-        passwordField : 'password',
-        passReqToCallback : true // allows us to pass back the entire request to the callback
-    },
-    function(req, email, password, done) {
+	passport.use('local-signup', new LocalStrategy({
+		// by default, local strategy uses username and password, we will override with email
+		usernameField : 'email',
+		passwordField : 'password',
+		passReqToCallback : true // allows us to pass back the entire request to the callback
+	},
+	function(req, email, password, done) {
 
 		// find a user whose email is the same as the forms email
 		// we are checking to see if the user trying to login already exists
-        User.findOne({ 'local.email' :  email }, function(err, user) {
-            // if there are any errors, return the error
-            if (err)
-                return done(err);
+		User.findOne({ 'local.email' :  email }, function(err, user) {
+			// if there are any errors, return the error
+			if (err)
+				return done(err);
 
-            // check to see if theres already a user with that email
-            if (user) {
-                return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
-            } else {
-                var name = req.body.firstName + " " + req.body.lastName;
-                var accountType = req.body.accountType;
+			// check to see if theres already a user with that email
+			if (user) {
+				return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+			} else {
+				var name = req.body.firstName + " " + req.body.lastName;
+				var accountType = req.body.accountType;
 				// if there is no user with that email
-                // create the user
-                var newUser = new User();
+				// create the user
+				var newUser = new User();
 
-                // set the user's local credentials
-                newUser.local.accountType = accountType;
-                newUser.local.name = name;
-                newUser.local.email = email;
-                newUser.local.password = newUser.generateHash(password);
+				// set the user's local credentials
+				newUser.local.accountType = accountType;
+				newUser.local.name = name;
+				newUser.local.email = email;
+				newUser.local.password = newUser.generateHash(password);
 
 				// save the user
-                newUser.save(function(err) {
-                    if (err)
-                        throw err;
-                    console.log("Successfully added new user to database");
-                    User.find({'local.email': email}, function(e, docs){console.log(docs);});
-                    req.session.userId = email;
-                    req.session.name = name;
-                    req.session.accountType = accountType;
-                    return done(null, newUser);
-                });
-            }
-        });        
-    }));
+				newUser.save(function(err) {
+					if (err)
+						throw err;
+					console.log("Successfully added new user to database");
+					User.find({'local.email': email}, function(e, docs){console.log(docs);});
+					req.session.userId = email;
+					req.session.name = name;
+					req.session.accountType = accountType;
+					return done(null, newUser);
+				});
+			}
+		});        
+	}));
 
-    // =========================================================================
-    // LOCAL LOGIN =============================================================
-    // =========================================================================
-    // we are using named strategies since we have one for login and one for signup
-    // by default, if there was no name, it would just be called 'local'
+	// =========================================================================
+	// LOCAL LOGIN =============================================================
+	// =========================================================================
+	// we are using named strategies since we have one for login and one for signup
+	// by default, if there was no name, it would just be called 'local'
 
-    passport.use('local-login', new LocalStrategy({
-        // by default, local strategy uses username and password, we will override with email
-        usernameField : 'email',
-        passwordField : 'password',
-        passReqToCallback : true // allows us to pass back the entire request to the callback
-    },
-    function(req, email, password, done) { // callback with email and password from our form
+	passport.use('local-login', new LocalStrategy({
+		// by default, local strategy uses username and password, we will override with email
+		usernameField : 'email',
+		passwordField : 'password',
+		passReqToCallback : true // allows us to pass back the entire request to the callback
+	},
+	function(req, email, password, done) { // callback with email and password from our form
 
-        // find a user whose email is the same as the forms email
-        // we are checking to see if the user trying to login already exists
-        User.findOne({ 'local.email' :  email }, function(err, user) {
-            // if there are any errors, return the error before anything else
-            if (err)
-                return done(err);
+		// find a user whose email is the same as the forms email
+		// we are checking to see if the user trying to login already exists
+		User.findOne({ 'local.email' :  email }, function(err, user) {
+			// if there are any errors, return the error before anything else
+			if (err)
+				return done(err);
 
-            // if no user is found, return the message
-            if (!user)
-                return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+			// if no user is found, return the message
+			if (!user)
+				return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
 
-            // if the user is found but the password is wrong
-            if (!user.validPassword(password))
-                return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+			// if the user is found but the password is wrong
+			if (!user.validPassword(password))
+				return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
 
-            req.session.userId = email;
-            req.session.name = user.local.name;
-            req.session.accountType = user.local.accountType;
-            // all is well, return successful user
-            return done(null, user);
-        });
-    }));
+			req.session.userId = email;
+			req.session.name = user.local.name;
+			req.session.accountType = user.local.accountType;
+			// all is well, return successful user
+			return done(null, user);
+		});
+	}));
 
-    passport.use('google-login', new GoogleStrategy({
-        clientID: '693576074665.apps.googleusercontent.com',
-        clientSecret: '693576074665@developer.gserviceaccount.com',
-        callbackURL: "http://127.0.0.1:3000/auth/google/callback"
-      },
-      function(accessToken, refreshToken, profile, done) {
-        User.findOrCreate({ googleId: profile.id }, function (err, user) {
-          return done(err, user);
-        });
-      }
-));
+	passport.use('google-login', new GoogleStrategy({
+		clientID: '693576074665.apps.googleusercontent.com',
+		clientSecret: '693576074665@developer.gserviceaccount.com',
+		callbackURL: 'http://127.0.0.1:3000/auth/google/callback',
+		scope: 'email' 
+	 },
+	function(accessToken, refreshToken, profile, done) {
+		User.findOrCreate({ googleId: profile.id }, function (err, user) {
+			return done(err, user);
+		});
+	}));
 };
