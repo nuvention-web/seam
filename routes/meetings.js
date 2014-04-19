61f00f5a22f383c4e06728c2c3b23bde8318b4d5
MikeTYChen <michael753@gmail.com>
2014-04-19 00:22:52 -0500

    tasks in minutes

--- routes/meetings.js
+++ routes/meetings.js
@@ -320,6 +320,7 @@ exports.endMeeting = function(req, res){
 	var meetingId = req.session.meetingId;
 	var mailBody, smtpConfig;
 	var emailAgenda='';
+	var emailTask='';
 	var agenda,meetingTitle,meetingDate,objective,meetingAttendees,emailDate,emailTime,creatorEmail;
 	var emailList='';
 	// console.log(meetingId);
@@ -337,9 +338,9 @@ exports.endMeeting = function(req, res){
 			objective=doc.objective;
 			meetingAttendees=doc.attendees;
 			meetingDate=doc.meetingDate;
-			var creatorEmail= getCreatorEmail(req,doc.userId);
+			var creatorEmail= req.session.userId;
 
-			console.log("EMAIL: "+creatorEmail);
+			console.log("EMAIL creator: "+creatorEmail);
 			emailList=creatorEmail+',';
 			var meetingYear = meetingDate.getFullYear(); 
 			var meetingMonth = meetingDate.getMonth()+1; 
@@ -354,24 +355,32 @@ exports.endMeeting = function(req, res){
 					if(agenda[i] != ''){
 						var number= i+1;
 						var notes= agenda[i].notes;
+						var tasks= agenda[i].tasks;
 						emailAgenda+=number+':  '+ agenda[i].topic+'<br/>';
 						var initialNoteCount= -1; //If there is initial note count =0
 						if(notes.length>0 && notes[0].notes!='' ){
 							emailAgenda+="<p style='margin-left:5em;'> A"+". "+notes[0].notes+"<br/></p>";
 							initialNoteCount=0;
 						}
+						if(tasks.length>0 && tasks[0].task!='' ){
+							emailTask+="<p style=''> TASK ("+tasks[0].assigneeName+"): "+tasks[0].task+". Due: "+tasks[0].taskDueDate+"<br/></p>";
+						}
 						for(var z=1; z<notes.length;z++){
 							emailAgenda+="<p style='margin-left:5em;'> " +String.fromCharCode(97 + z+ initialNoteCount)+". "+notes[z].notes+"<br/></p>";
+						}
+						for(var z=1; z<tasks.length;z++){
+							emailTask+="<p style=''> TASK ("+tasks[z].assigneeName+"): "+tasks[z].task+". Due: "+tasks[z].taskDueDate+"<br/></p>";
 						}
 					}
 				};
 			}
+			console.log("EMAIL: "+ emailTask);
 			if(meetingAttendees!=''){
 				for(var i=0; i<meetingAttendees.length; i++){
 					emailList+=meetingAttendees[i].attendeeEmail+',';
 				}
 			}
-			mailBody=createMinutesBody(emailDate,meetingTitle,emailList,objective,emailAgenda,emailTime,objective,emailAgenda);
+			mailBody=createMinutesBody(emailDate,meetingTitle,emailList,objective,emailAgenda,emailTask,emailTime,objective,emailAgenda);
 			emailFunction(mailBody,res);
 
 		});
@@ -701,7 +710,7 @@ function createAgendaBody(emailList,emailDate,meetingTitle,objective,emailAgenda
 
 	// HTML body
      	html:"<body>"+
-     	"<p style='text-align:center'><img src='cid:logo@seam'/></p>"+
+     	"<p style='text-align:center; margin:0 auto;  width:50px; height:50px;'><img src='cid:logo@seam'/></p>"+
         "<p style='text-align:left;'> Date: "+emailDate+"<br/></p>" +
         "<p style='text-align:left;'> Location: "+location+"<br/></p>" +
         "<p style='text-align:left;'> Duration: "+meetingTime+" Minutes <br/></p>" +
@@ -711,7 +720,7 @@ function createAgendaBody(emailList,emailDate,meetingTitle,objective,emailAgenda
         attachments:[
          // Logo img
 	        {
-             filePath: './public/images/seamlogo-red125.png',
+             filePath: './public/images/favicon-64.png',
              cid: 'logo@seam' // should be as unique as possible
          },
         {
@@ -723,7 +732,7 @@ function createAgendaBody(emailList,emailDate,meetingTitle,objective,emailAgenda
  };
  return mailBody;
 }
-function createMinutesBody(emailDate,meetingTitle,emailList,objective,emailAgenda,emailTime,objective,emailAgenda){
+function createMinutesBody(emailDate,meetingTitle,emailList,objective,emailAgenda,emailTask,emailTime,objective,emailAgenda){
 	var mailBody;
 	//construct the email sending module
 			mailBody = {
@@ -735,15 +744,16 @@ function createMinutesBody(emailDate,meetingTitle,emailList,objective,emailAgend
 
 			// HTML body
 		     	html:"<body>"+
-		     	"<p style='text-align:center'><img src='cid:logo@seam'/></p>"+
+		     	"<p style='text-align:center; margin:0 auto; width:50px; height:50px; '><img src='cid:logo@seam'/></p>"+
 		        "<p style='text-align:left;'> Duration: "+emailTime+" Minutes<br/></p>" +
 		        "<p style='text-align:left;'> Objectives: "+objective+"<br/></p>" +
+		        "<p style='text-align:left;'> Tasks: "+emailTask+"</p>" +
 		        "<p style='text-align:left;'> Agenda: <br/>"+emailAgenda+"<br/></p>"+ 
 		        "</body>",
 		        attachments:[
 		         // Logo img
 			        {
-		             filePath: './public/images/seamlogo-red125.png',
+		             filePath: './public/images/favicon-64.png',
 		             cid: 'logo@seam' // should be as unique as possible
 		         }
 		    	]
