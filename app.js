804824a4c3d9b208ef7fa3734ef052f7de7901ad
jyu3138 <tyu3138@gmail.com>
2014-04-20 14:16:18 -0500

    connecting to socket.io

--- app.js
+++ app.js
@@ -66,6 +66,13 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+//server listening
+var server = app.listen(app.get('port'), function(){
+	console.log('Express server listening on port ' + app.get('port'));
+});
+
+var io = require('socket.io').listen(server);
+
 //Landing Page
 app.get('/', landingPage.home);
 app.get('/home', landingPage.home);
@@ -151,7 +158,3 @@ app.get('/auth/google/callback',  passport.authenticate('google-login', {
 }))
 //error page
 app.get('/error', index.error);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
