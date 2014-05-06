43a55f2dc2711fdfb2e24171045f4e31fcf36049
MikeTYChen <michael753@gmail.com>
2014-05-06 13:22:29 -0500

    login page

--- public/javascripts/landingPage.js
+++ public/javascripts/landingPage.js
@@ -1,24 +1,5 @@
  $(document).ready(function(){ 
 
-    if(window.innerWidth >= 800 && window.innerHeight >= 600) {
-    //Navbar Color Change Function
-    var scroll = 0;            
-    $(document).scroll(function() { 
-       // var changePos=$("#home-page").height()-1000;
-       var changePos=100;
-        scroll = $(this).scrollTop();
-        if(scroll > changePos) {
-            var progress= $('.progress').css('width');
-            var progressFrustration = $('.progress-bar-success').css('width')
-            $(".navbar").css('background-color', 'rgba(0,0,0,.8)');
-            $(".navbar").css('background-color', 'rgba(0,0,0,.8)');
-            $(".navbar").css('transition', 'background-color 1.5s ease-in-out');
-        } 
-        else {
-            $(".navbar").css('background-color', 'transparent');
-        }
-    });
-}
     //Navbar Login Fade-In-Out Function
     $('#admin-btn').click(function(){
         $('.visible-login').fadeOut(300);
