3c1b3930ca67d55bb9022cc2e8d1b684889c4ba5
MikeTYChen <michael753@gmail.com>
2014-05-13 11:33:05 -0500

    ui logged in fixes

--- public/javascripts/meetings-addAgenda.js
+++ public/javascripts/meetings-addAgenda.js
@@ -72,10 +72,10 @@ function addAgendaItem(){
         			'<div style="width:40px" class="bar"></div>'+
       					'</div></div></div>'+
 		'<div class="col-md-7 margin-0 padding-0">'+
-    		'<input id="makeMeeting'+i+'" type="text" name="agendaTopic" placeholder="ADD AGENDA ITEM" autocomplete="off" class="text-h3 noEnterSubmit height-30x form-control border-square border-none text-left text-blue bg-transparent cursor-pointer"/>'+
+    		'<input id="makeMeeting'+i+'" type="text" name="agendaTopic" placeholder="Add agenda item" autocomplete="off" class="text-h3 noEnterSubmit height-30x form-control border-square border-none text-left text-blue bg-transparent cursor-pointer"/>'+
   				'</div>'+
   		'<div class="col-md-3">'+
-  				'<input id="duration'+i+'" type="number" name="duration" class=" text-h3 noEnterSubmit height-30x form-control border-square border-none text-right bg-transparent cursor-pointer" placeholder= "MINUTES" autocomplete="off" value="0" required/>'+
+  				'<input id="duration'+i+'" type="number" name="duration" class=" text-h3 noEnterSubmit height-30x form-control border-square border-none text-right bg-transparent cursor-pointer" placeholder= "Minutes" autocomplete="off" value="0" required/>'+
   				'</div>'+
   		'<div class="col-md-1 padding-0 text-center" style="margin-top:0.5%;">'+
   			'<a class="text-h2 text-normal text-gray-hover" href="#" onclick="removeAgenda(agendaItem'+ i + ');return false;"><i class="fa fa-times fa-md"></i> </a>'+
@@ -88,7 +88,7 @@ function addAgendaItem(){
        				'</div></div></div></div>'+
   		'<div class="col-md-9 margin-0 padding-0">'+
     		'<div class="width-95p margin-top-1p overflow-scroll-y-auto max-height-150x">'+
-     			'<textarea id="makeMeeting'+i+'" type="text" name="notes" rows="6" placeholder="INSERT NOTES HERE" autocomplete="off" class="text-h4 height-30x form-control border-square border-none text-left bg-transparent cursor-pointer"/>'+
+     			'<textarea id="makeMeeting'+i+'" type="text" name="notes" rows="6" placeholder="Insert notes here" autocomplete="off" class="text-h4 height-30x form-control border-square border-none text-left bg-transparent cursor-pointer"/>'+
      			'</div></div></div></div>').appendTo(agendaBox);
 	i++;
 };
