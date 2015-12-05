ed620f8b0338741bb03b92c3b0c8301086b4a074
Michael Chen <miketychen@gmail.com>
2015-12-05 10:00:45 -0600

    fix mongoose update

--- models/contact-model.js
+++ models/contact-model.js
@@ -1,14 +1,14 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var memberSchema= new Schema({
+	memberName: { type: String },
+	memberEmail: { type: String }
+});
+
 var contactSchema = new Schema({
 	UserId: { type: String, required : true},
 	contacts: [memberSchema]
 });
-
-var memberSchema= new Schema({
-	memberName: { type: String },
-	memberEmail: { type: String }
-})
 
 module.exports = mongoose.model('Contact', contactSchema);
